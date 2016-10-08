3bd6d9290b609e6fdca1059fad587ec0033628f6
陆离01 <luli01@snda.com>
2016-10-08 19:08:49 +0800

    Initial commit

---
+++ app/src/main/java/com/liyue/app/mycompass/MainActivity.java
@@ -0,0 +1,18 @@
+package com.liyue.app.mycompass;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CompassView cv = (CompassView)this.findViewById(R.id.compassView);
+        cv.setBearing(45);
+    }
+}
